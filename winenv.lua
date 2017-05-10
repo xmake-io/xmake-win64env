9f3d3c20f0b67059a92327bef0b45008acd5a667
ruki <waruqi@gmail.com>
2017-05-10 20:33:07 +0800

    improve winenv.lua

--- winenv.lua
+++ winenv.lua
@@ -1,5 +1,5 @@
 -- load winenv
 function main(rootdir)
     rootdir = rootdir or os.curdir()
-    os.setenv("PATH", (os.getenv("PATH") or "") .. ";" .. path.join(rootdir, "bin") .. ";" .. path.join(rootdir, "git", "cmd") .. ";" .. path.join(rootdir, "cmake", "bin"))
+    os.addenv("PATH", path.join(rootdir, "bin"), path.join(rootdir, "git", "cmd"), path.join(rootdir, "cmake", "bin"))
 end
